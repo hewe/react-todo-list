ba13b6abeb1a3bc184b644820af425beb0178088
Weiping He <weipingx.he@gmail.com>
2016-08-24 14:13:55 -0700

    Webpack transipile and bundel setup, created app container component, referened it in the html.

--- client/client.js
+++ client/client.js
@@ -1,1 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from '../components/App';
 
+render(
+    <App/>, /* the component that you want to render. In this case it is App component with JSX syntax. */
+    document.getElementById('app') /* This is the DOM element that you want to mount the component for the render */
+);
--- components/App.js
+++ components/App.js
@@ -1,1 +1,13 @@
+import React, { Component } from 'react';
 
+class App extends Component {
+
+    /**
+     * Every component has a render function that returns JSX
+     */
+    render() {
+        return <div> This is definitely a React app now!</div>
+    }
+}
+
+export default App;
